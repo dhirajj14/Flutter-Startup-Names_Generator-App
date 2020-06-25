f08a294da3f839260a1222527da87b4c4ca0a0bf
dhirajj14 <djain14@hawk.iit.edu>
2020-06-25 14:17:16 -0500

    display wordpair on screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -14,9 +13,22 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomNames(),
         ),
       ),
     );
+  }
+}
+
+class RandomNames extends StatefulWidget {
+  @override
+  _RandomNamesState createState() => _RandomNamesState();
+}
+
+class _RandomNamesState extends State<RandomNames> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
