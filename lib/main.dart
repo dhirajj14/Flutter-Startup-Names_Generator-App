1337d95b1b1bb9fbed8e0401fbc323dd60db375d
dhirajj14 <djain14@hawk.iit.edu>
2020-06-25 14:34:52 -0500

    create a listView to see all the names

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomNames(),
-        ),
-      ),
+      home: RandomNames(),
     );
   }
 }
@@ -26,9 +19,38 @@ class RandomNames extends StatefulWidget {
 }
 
 class _RandomNamesState extends State<RandomNames> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(itemBuilder: (BuildContext _context, int i) {
+      if (i.isOdd) {
+        return Divider();
+      }
+      final int index = i ~/ 2;
+
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
